b8b3e72bb82ceaa3fe50a59d8cf14f12c57e0818
Donghyuk Kang <kderr2791@gmail.con>
2018-06-22 13:25:53 +0900

    제이쿼리 연동

--- src/main/java/kr/co/cks/controller/HomeController.java
+++ src/main/java/kr/co/cks/controller/HomeController.java
@@ -26,14 +26,7 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "init";
 	}
 	
 }
